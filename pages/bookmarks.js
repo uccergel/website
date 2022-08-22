d341f09913e070b0ae260e08b20670b2526341e0
uccergel <ugurcemcergel@gmail.com>
2022-08-22 23:56:43 +0300

    giriş metni düzenlendi. İlk yazıl yazıldı.

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -2,6 +2,7 @@ export default function Bookmarks() {
   return (
     <div className="site-container">
       <h1 className="text-4xl">Bu alanı tasarlıyorum.</h1>
+        <p>Airtable üzerinde kaydettiğim ve beğendiğim linkleri paylaşacağım</p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,12 @@ function HomePage() {
       <>
       <div className="site-container">
       <div className="my-4">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold mb-6">
           <span>Merhaba,</span>
           <br />
-          <span className=" text-xl text-gray-500">İstanbul'da yaşıyorum.</span></h1>
-        <p>Ahşap ürün yapımına ve yazılıma ilgim var.</p>
+          <span className=" text-xl text-gray-500">İstanbul'da yaşayan Kimya Bölümü mezunu bir iletişimciyim.</span>
+        </h1>
+        <p>Ahşap ürün yapımına, özellikle kuksa bardak yapımı, ve yazılıma ilgim var.</p>
         <p>Yazılıma olan merakımla Bilgisayar Mühendisliği okuyorum.</p>
         <p>Öğrendiğim bilgileri, takip ettiğim kişileri ve beğendiğim paylaşımları bu site üzerinden paylaşmayı planlıyorum.</p>
       </div>
